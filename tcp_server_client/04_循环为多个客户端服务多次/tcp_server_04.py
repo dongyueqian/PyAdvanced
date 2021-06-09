87d2597271720613e78a594fb017dae92953d439
dongyueqian <874974405@qq.com>
2021-06-09 22:20:54 +0800

    用多线程实现服务器为多个tcp客户端服务

--- tcp_server_client/04_循环为多个客户端服务多次/tcp_server_04.py
+++ tcp_server_client/04_循环为多个客户端服务多次/tcp_server_04.py
@@ -9,7 +9,7 @@ import threading
 def dealClient(new_socket,client_addr):
     # 循环为一个客户端服务多次
     while True:
-        # 5、接收客户端发送过来的请求
+        # 6、接收客户端发送过来的请求
         recv_data = new_socket.recv(1024)
 
         # 如果recv解堵塞，那么有2种方式：
@@ -17,11 +17,11 @@ def dealClient(new_socket,client_addr):
         # 2.客户端调用close导致这里的recv解堵塞
         if recv_data:
             print("客户端%s发送的请求是:%s" % (client_addr, recv_data.decode("utf-8")))
-            # 6、给客户端返回数据
+            # 7、给客户端返回数据
             new_socket.send("========你好==========".encode("utf-8"))
         else:
             break
-    # 关闭accept返回的套接字，不再为这个客户端服务
+    # 8、关闭accept返回的套接字，不再为这个客户端服务
     print("为%s服务结束" % (client_addr,))
     new_socket.close()
 
@@ -32,6 +32,7 @@ def main():
 
     # 2、绑定IP和port
     tcp_server_socket.bind(("", 8888))
+
     # 3、变为监听套接字
     tcp_server_socket.listen(128)
 
@@ -48,7 +49,7 @@ def main():
             t.start()
 
     except KeyboardInterrupt:
-        # 8、关闭监听套接字
+        # 9、关闭监听套接字
         tcp_server_socket.close()
         print("服务器关闭")
 
